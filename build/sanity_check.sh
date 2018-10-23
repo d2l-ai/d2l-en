c05548a9cfb5818face6e0e64a61aec20623ad7b
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2018-10-23 11:26:07 -0700

    sanity check: allow flexible math, handle md output from Jupyter (#14)
    * sanity check: allow flexible math, handle md output from Jupyter
    
    * update
    
    * tab to space

--- build/sanity_check.sh
+++ build/sanity_check.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 set -e
 
-# check no ipynb files
+# Check no ipynb files.
 IPYNB=`find chapter* -type f -name "*.ipynb"`
 if [[ ! -z "$IPYNB" ]]; then
     echo "ERROR: Find the following .ipynb files. "
     echo "$IPYNB"
     echo "ERROR: You should convert them into markdown files. "
     echo "ERROR: You can do it with by 'jupyter nbconvert --to markdown your.ipynb'"
-    # exit -1
+    exit -1
 fi
 
 
-# check no output
+# Check no output.
 HAS_OUTPUT=0
 for FNAME in `find chapter* -type f -name "*.md"`; do
     OUTPUT=""
     IN_CODE=0
+    IN_JSON_OUTPUT=0
     IFS=''
     L=1
     while read LINE; do
-        if [[ "$LINE" =~ ^\`\`\`.* ]]; then
-            IN_CODE=$(($IN_CODE ^ 1))
+        if [[ "$LINE" =~ ^\`\`\`\{\.json\ \.output ]]; then
+            IN_JSON_OUTPUT=1
+        elif [[ "$LINE" =~ ^\`\`\`.* ]]; then
+            if [[ "$IN_JSON_OUTPUT" == "1" ]]; then
+                IN_JSON_OUTPUT=0
+            else
+                IN_CODE=$(($IN_CODE ^ 1))
+            fi
         fi
 
-        if [[ "$LINE" =~ ^\$\$.* ]] && ! [[ "$LINE" =~ .+\$\$$ ]]; then
-            IN_CODE=$(($IN_CODE ^ 1))
+        # Skip one-line math.
+        if ! [[ "$LINE" =~ ^\$\$.+ ]] || ! [[ "$LINE" =~ .+\$\$$ ]]; then
+        # Math also allows multiple space at the beginning of a line.
+            if [[ "$LINE" =~ ^\$\$.* ]] || [[ "$LINE" =~ .*\$\$$ ]]; then
+                IN_CODE=$(($IN_CODE ^ 1))
+            fi
         fi
 
-        if [[ "$LINE" =~ ^\ \ \ .* ]] && [[ "$IN_CODE" == "0" ]]; then
+
+        if [[ "$LINE" =~ ^\ \ \ .* ]] && [[ "$IN_CODE" == "0" ]] || [[ "$IN_JSON_OUTPUT" == "1" ]]; then
             echo $FNAME $LINE
             OUTPUT=$(printf "$OUTPUT\nL$L: $LINE")
         fi
