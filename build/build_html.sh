becaf8487f8a906d17fff45d77243e06faaf93f5
Mu Li <mli@amazon.com>
2018-11-12 13:50:33 -0800

    Update build_html.sh

--- build/build_html.sh
+++ build/build_html.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
-echo ${LD_LIBRARY_PATH}
-echo ${PATH}
 
 conda env update -f build/env.yml
 conda activate d2l-en-build
