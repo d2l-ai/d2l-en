0538203962c720dfc36263b8bd5c0cbed631bd34
mli <muli@cs.cmu.edu>
2018-11-28 20:08:41 +0000

    add a large expire time to fonts

--- build/upload.sh
+++ build/upload.sh
@@ -9,8 +9,7 @@ BUCKET=s3://diveintodeeplearning.org
 
 DIR=build/_build/html/
 
-find $DIR \( -iname '*.css' -o -iname '*.js' \) \
-     -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+find $DIR \( -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
 
 aws s3 sync --exclude '*.*' --include '*.css' \
       --content-type 'text/css' \
@@ -18,6 +17,10 @@ aws s3 sync --exclude '*.*' --include '*.css' \
       --acl 'public-read' \
       $DIR $BUCKET
 
+aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
+      --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
+      --acl 'public-read' \
+      $DIR $BUCKET
 
 aws s3 sync --exclude '*.*' --include '*.js' \
       --content-type 'application/javascript' \
@@ -27,4 +30,4 @@ aws s3 sync --exclude '*.*' --include '*.js' \
 
 aws s3 sync --delete $DIR $BUCKET --acl 'public-read'
 
-# TODO: add expire and cache control
+# TODO: add cache control
