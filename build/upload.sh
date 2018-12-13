7fd5936986f19df202b06614c4d919ca32ff1d4e
Aston Zhang <asv325@gmail.com>
2018-12-13 23:01:22 +0000

    s3 quiet and rm build html, pdf, pkg

--- build/upload.sh
+++ build/upload.sh
@@ -18,7 +18,7 @@ DIR=build/_build/html/
 
 aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
       --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
-      --acl 'public-read' \
+      --acl 'public-read' --quiet \
       $DIR $BUCKET
 
 #aws s3 sync --exclude '*.*' --include '*.js' \
@@ -27,7 +27,7 @@ aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
 #      --acl 'public-read' \
 #      $DIR $BUCKET
 
-aws s3 sync --delete $DIR $BUCKET --acl 'public-read'
+aws s3 sync --delete $DIR $BUCKET --acl 'public-read' --quiet
 
 echo 's3 uploaded'
 
