c526c9752bacb299098270a17b7060da08514e14
mli <muli@cs.cmu.edu>
2018-12-27 22:24:41 +0000

    fix ci

--- build/publish_notebook.sh
+++ build/publish_notebook.sh
@@ -6,8 +6,8 @@ conda activate d2l-en-build
 git submodule update --init
 
 DIR=../d2l-en-notebooks
-build/utils/notebooks_no_output.sh . ${DIR}
-rm ${DIR}/*.md
+# build/utils/notebooks_no_output.sh . ${DIR}
+rm ${DIR}/*.ipynb
 cp environment.yml ${DIR}/
 
 build/utils/upload_github.sh ${DIR} d2l-ai/notebooks
