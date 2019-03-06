0a81569f57bd1c816e9fcf6539cf7e078d0549a0
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2019-03-06 00:00:01 +0000

    Revert "docker install (#169)" (#170)
    This reverts commit bab0e83563dfdc72ab82b415eb84051ad6775ed3.

--- docker/run_conda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash --login
-
-source /root/.bashrc
-conda activate gluon
-/opt/conda/bin/jupyter notebook --notebook-dir=/d2l-en --ip='0.0.0.0' --port=8888 --no-browser --allow-root --NotebookApp.token='d2l'
