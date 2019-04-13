c53d634e9eef64cb467786270e505366860b880a
mli <muli@cs.cmu.edu>
2019-04-13 02:32:14 +0000

    more d2l

---
+++ d2l/train.py
@@ -0,0 +1,328 @@
+"""The train modules contains functions for neural network training"""
+import numpy as np
+import math
+import time
+
+import mxnet as mx
+from mxnet import autograd, gluon, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn, utils as gutils
+from .data import data_iter_consecutive, data_iter_random
+from .base import try_gpu
+from .figure import set_figsize, plt
+from .model import linreg
+
+__all__ = ['evaluate_accuracy', 'squared_loss', 'grad_clipping', 'sgd', 'train',
+           'train_2d', 'train_and_predict_rnn', 'train_and_predict_rnn_gluon',
+           'train_ch3', 'train_ch5', 'train_ch7', 'train_gluon_ch7', 'predict_sentiment']
+
+def _get_batch(batch, ctx):
+    """Return features and labels on ctx."""
+    features, labels = batch
+    if labels.dtype != features.dtype:
+        labels = labels.astype(features.dtype)
+    return (gutils.split_and_load(features, ctx),
+            gutils.split_and_load(labels, ctx), features.shape[0])
+
+def evaluate_accuracy(data_iter, net, ctx=[mx.cpu()]):
+    """Evaluate accuracy of a model on the given data set."""
+    if isinstance(ctx, mx.Context):
+        ctx = [ctx]
+    acc_sum, n = nd.array([0]), 0
+    for batch in data_iter:
+        features, labels, _ = _get_batch(batch, ctx)
+        for X, y in zip(features, labels):
+            y = y.astype('float32')
+            acc_sum += (net(X).argmax(axis=1) == y).sum().copyto(mx.cpu())
+            n += y.size
+        acc_sum.wait_to_read()
+    return acc_sum.asscalar() / n
+
+def squared_loss(y_hat, y):
+    """Squared loss."""
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+def grad_clipping(params, theta, ctx):
+    """Clip the gradient."""
+    if theta is not None:
+        norm = nd.array([0], ctx)
+        for param in params:
+            norm += (param.grad ** 2).sum()
+        norm = norm.sqrt().asscalar()
+        if norm > theta:
+            for param in params:
+                param.grad[:] *= theta / norm
+
+def sgd(params, lr, batch_size):
+    """Mini-batch stochastic gradient descent."""
+    for param in params:
+        param[:] = param - lr * param.grad / batch_size
+
+def train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs):
+    """Train and evaluate a model."""
+    print('training on', ctx)
+    if isinstance(ctx, mx.Context):
+        ctx = [ctx]
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, m, start = 0.0, 0.0, 0, 0, time.time()
+        for i, batch in enumerate(train_iter):
+            Xs, ys, batch_size = _get_batch(batch, ctx)
+            ls = []
+            with autograd.record():
+                y_hats = [net(X) for X in Xs]
+                ls = [loss(y_hat, y) for y_hat, y in zip(y_hats, ys)]
+            for l in ls:
+                l.backward()
+            trainer.step(batch_size)
+            train_l_sum += sum([l.sum().asscalar() for l in ls])
+            n += sum([l.size for l in ls])
+            train_acc_sum += sum([(y_hat.argmax(axis=1) == y).sum().asscalar()
+                                  for y_hat, y in zip(y_hats, ys)])
+            m += sum([y.size for y in ys])
+        test_acc = evaluate_accuracy(test_iter, net, ctx)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
+              'time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / m, test_acc,
+                 time.time() - start))
+
+
+def train_2d(trainer):
+    """Optimize the objective function of 2d variables with a customized trainer."""
+    x1, x2 = -5, -2
+    s_x1, s_x2 = 0, 0
+    res = [(x1, x2)]
+    for i in range(20):
+        x1, x2, s_x1, s_x2 = trainer(x1, x2, s_x1, s_x2)
+        res.append((x1, x2))
+    print('epoch %d, x1 %f, x2 %f' % (i+1, x1, x2))
+    return res
+
+
+
+def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
+                          vocab_size, ctx, corpus_indices, idx_to_char,
+                          char_to_idx, is_random_iter, num_epochs, num_steps,
+                          lr, clipping_theta, batch_size, pred_period,
+                          pred_len, prefixes):
+    """Train an RNN model and predict the next item in the sequence."""
+    if is_random_iter:
+        data_iter_fn = data_iter_random
+    else:
+        data_iter_fn = data_iter_consecutive
+    params = get_params()
+    loss = gloss.SoftmaxCrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        if not is_random_iter:
+            state = init_rnn_state(batch_size, num_hiddens, ctx)
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, ctx)
+        for X, Y in data_iter:
+            if is_random_iter:
+                state = init_rnn_state(batch_size, num_hiddens, ctx)
+            else:
+                for s in state:
+                    s.detach()
+            with autograd.record():
+                inputs = [nd.one_hot(x, vocab_size) for x in X.T]
+                (outputs, state) = rnn(inputs, state, params)
+                outputs = nd.concat(*outputs, dim=0)
+                y = Y.T.reshape((-1,))
+                l = loss(outputs, y).mean()
+            l.backward()
+            grad_clipping(params, clipping_theta, ctx)
+            sgd(params, lr, 1)
+            l_sum += l.asscalar() * y.size
+            n += y.size
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            for prefix in prefixes:
+                print(' -', predict_rnn(
+                    prefix, pred_len, rnn, params, init_rnn_state,
+                    num_hiddens, vocab_size, ctx, idx_to_char, char_to_idx))
+
+
+def train_and_predict_rnn_gluon(model, num_hiddens, vocab_size, ctx,
+                                corpus_indices, idx_to_char, char_to_idx,
+                                num_epochs, num_steps, lr, clipping_theta,
+                                batch_size, pred_period, pred_len, prefixes):
+    """Train an Gluon RNN model and predict the next item in the sequence."""
+    loss = gloss.SoftmaxCrossEntropyLoss()
+    model.initialize(ctx=ctx, force_reinit=True, init=init.Normal(0.01))
+    trainer = gluon.Trainer(model.collect_params(), 'sgd',
+                            {'learning_rate': lr, 'momentum': 0, 'wd': 0})
+
+    for epoch in range(num_epochs):
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_consecutive(
+            corpus_indices, batch_size, num_steps, ctx)
+        state = model.begin_state(batch_size=batch_size, ctx=ctx)
+        for X, Y in data_iter:
+            for s in state:
+                s.detach()
+            with autograd.record():
+                (output, state) = model(X, state)
+                y = Y.T.reshape((-1,))
+                l = loss(output, y).mean()
+            l.backward()
+            params = [p.data() for p in model.collect_params().values()]
+            grad_clipping(params, clipping_theta, ctx)
+            trainer.step(1)
+            l_sum += l.asscalar() * y.size
+            n += y.size
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            for prefix in prefixes:
+                print(' -', predict_rnn_gluon(
+                    prefix, pred_len, model, vocab_size, ctx, idx_to_char,
+                    char_to_idx))
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, trainer=None):
+    """Train and evaluate a model with CPU."""
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            with autograd.record():
+                y_hat = net(X)
+                l = loss(y_hat, y).sum()
+            l.backward()
+            if trainer is None:
+                sgd(params, lr, batch_size)
+            else:
+                trainer.step(batch_size)
+            y = y.astype('float32')
+            train_l_sum += l.asscalar()
+            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
+            n += y.size
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+def train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx,
+              num_epochs):
+    """Train and evaluate a model with CPU or GPU."""
+    print('training on', ctx)
+    loss = gloss.SoftmaxCrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X, y = X.as_in_context(ctx), y.as_in_context(ctx)
+            with autograd.record():
+                y_hat = net(X)
+                l = loss(y_hat, y).sum()
+            l.backward()
+            trainer.step(batch_size)
+            y = y.astype('float32')
+            train_l_sum += l.asscalar()
+            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
+            n += y.size
+        test_acc = evaluate_accuracy(test_iter, net, ctx)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
+              'time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc,
+                 time.time() - start))
+
+
+def train_ch7(trainer_fn, states, hyperparams, features, labels, batch_size=10,
+              num_epochs=2):
+    """Train a linear regression model."""
+    net, loss = linreg, squared_loss
+    w, b = nd.random.normal(scale=0.01, shape=(
+        features.shape[1], 1)), nd.zeros(1)
+    w.attach_grad()
+    b.attach_grad()
+
+    def eval_loss():
+        return loss(net(features, w, b), labels).mean().asscalar()
+
+    ls = [eval_loss()]
+    data_iter = gdata.DataLoader(
+        gdata.ArrayDataset(features, labels), batch_size, shuffle=True)
+    for _ in range(num_epochs):
+        start = time.time()
+        for batch_i, (X, y) in enumerate(data_iter):
+            with autograd.record():
+                l = loss(net(X, w, b), y).mean()
+            l.backward()
+            trainer_fn([w, b], states, hyperparams)
+            if (batch_i + 1) * batch_size % 100 == 0:
+                ls.append(eval_loss())
+    print('loss: %f, %f sec per epoch' % (ls[-1], time.time() - start))
+    set_figsize()
+    plt.plot(np.linspace(0, num_epochs, len(ls)), ls)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+
+
+def train_gluon_ch7(trainer_name, trainer_hyperparams, features, labels,
+                    batch_size=10, num_epochs=2):
+    """Train a linear regression model with a given Gluon trainer."""
+    net = nn.Sequential()
+    net.add(nn.Dense(1))
+    net.initialize(init.Normal(sigma=0.01))
+    loss = gloss.L2Loss()
+
+    def eval_loss():
+        return loss(net(features), labels).mean().asscalar()
+
+    ls = [eval_loss()]
+    data_iter = gdata.DataLoader(
+        gdata.ArrayDataset(features, labels), batch_size, shuffle=True)
+    trainer = gluon.Trainer(net.collect_params(),
+                            trainer_name, trainer_hyperparams)
+    for _ in range(num_epochs):
+        start = time.time()
+        for batch_i, (X, y) in enumerate(data_iter):
+            with autograd.record():
+                l = loss(net(X), y)
+            l.backward()
+            trainer.step(batch_size)
+            if (batch_i + 1) * batch_size % 100 == 0:
+                ls.append(eval_loss())
+    print('loss: %f, %f sec per epoch' % (ls[-1], time.time() - start))
+    set_figsize()
+    plt.plot(np.linspace(0, num_epochs, len(ls)), ls)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+
+def predict_rnn(prefix, num_chars, rnn, params, init_rnn_state,
+                num_hiddens, vocab_size, ctx, idx_to_char, char_to_idx):
+    """Predict next chars with a RNN model"""
+    state = init_rnn_state(1, num_hiddens, ctx)
+    output = [char_to_idx[prefix[0]]]
+    for t in range(num_chars + len(prefix) - 1):
+        X = [nd.one_hot(x, vocab_size) for x in nd.array([output[-1]], ctx=ctx).T]
+        (Y, state) = rnn(X, state, params)
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t + 1]])
+        else:
+            output.append(int(Y[0].argmax(axis=1).asscalar()))
+    return ''.join([idx_to_char[i] for i in output])
+
+
+def predict_rnn_gluon(prefix, num_chars, model, vocab_size, ctx, idx_to_char,
+                      char_to_idx):
+    """Precit next chars with a Gluon RNN model"""
+    state = model.begin_state(batch_size=1, ctx=ctx)
+    output = [char_to_idx[prefix[0]]]
+    for t in range(num_chars + len(prefix) - 1):
+        X = nd.array([output[-1]], ctx=ctx).reshape((1, 1))
+        (Y, state) = model(X, state)
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t + 1]])
+        else:
+            output.append(int(Y.argmax(axis=1).asscalar()))
+    return ''.join([idx_to_char[i] for i in output])
+
+
+def predict_sentiment(net, vocab, sentence):
+    """Predict the sentiment of a given sentence."""
+    sentence = nd.array(vocab.to_indices(sentence), ctx=try_gpu())
+    label = nd.argmax(net(sentence.reshape((1, -1))), axis=1)
+    return 'positive' if label.asscalar() == 1 else 'negative'
