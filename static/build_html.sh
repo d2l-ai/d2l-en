e42758778ab3abb8249862ff2622bc32a0b143c7
muli <muli@cs.cmu.edu>
2019-04-29 13:43:09 -0700

    optimize svg zoom in

--- static/build_html.sh
+++ static/build_html.sh
@@ -7,10 +7,10 @@ d2lbook build html
 cp static/frontpage/_images/* _build/html/_images/
 
 for fn in `find _build/html/_images/ -iname '*.svg' `; do
-    if [[ $fn == *'qr_'* ]]; then
+    if [[ $fn == *'qr_'* ]] || [[ $fn == *'output_'* ]]; then
         continue
     fi
-    echo "Zoom in $fn by 1.3x"
-    rsvg-convert -z 1.3 -f svg -o tmp.svg $fn
+    echo "Zoom in $fn by 1.15x"
+    rsvg-convert -z 1.15 -f svg -o tmp.svg $fn
     mv tmp.svg $fn
 done
