faf19125fcae219a43fdf653963c3d536a73ea24
muli <muli@cs.cmu.edu>
2019-06-07 14:19:35 -0700

    update d2l

--- d2l/__init__.py
+++ d2l/__init__.py
@@ -1,10 +1,9 @@
-from .d2l import *
 from .base import *
 from .figure import *
 from .data import *
 from .model import *
 from .train import *
 
-from .preface import *
+from .d2l import *
 
 __version__ = '0.9.3'
