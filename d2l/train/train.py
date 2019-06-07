e9373356825bb77314c8e20973e6249d09be25e8
muli <muli@cs.cmu.edu>
2019-06-07 23:37:42 +0000

    clean train.py

--- d2l/train/train.py
+++ d2l/train/train.py
@@ -190,33 +190,6 @@ def train_and_predict_rnn_gluon(model, num_hiddens, corpus_indices, vocab,
             for prefix in prefixes:
                 print(' -', predict_rnn_gluon(prefix, 50, model, vocab, ctx))
 
-
-
-def train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx,
-              num_epochs):
-    """Train and evaluate a model with CPU or GPU."""
-    print('training on', ctx)
-    loss = gloss.SoftmaxCrossEntropyLoss()
-    for epoch in range(num_epochs):
-        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
-        for X, y in train_iter:
-            X, y = X.as_in_context(ctx), y.as_in_context(ctx)
-            with autograd.record():
-                y_hat = net(X)
-                l = loss(y_hat, y).sum()
-            l.backward()
-            trainer.step(batch_size)
-            y = y.astype('float32')
-            train_l_sum += l.asscalar()
-            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
-            n += y.size
-        test_acc = evaluate_accuracy(test_iter, net, ctx)
-        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, '
-              'time %.1f sec'
-              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc,
-                 time.time() - start))
-
-
 def train_ch9(trainer_fn, states, hyperparams, features, labels, batch_size=10,
               num_epochs=2):
     """Train a linear regression model."""
