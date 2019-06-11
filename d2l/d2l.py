b89d58217ab5a019b21e8f64bca3ca5cd5ee7b2b
muli <muli@cs.cmu.edu>
2019-06-11 23:49:50 +0000

    detection

--- d2l/d2l.py
+++ d2l/d2l.py
@@ -550,6 +550,35 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
                       va='center', ha='center', fontsize=9, color=text_color,
                       bbox=dict(facecolor=color, lw=0))
 
+# Defined in file: ./chapter_computer-vision/object-detection-dataset.md
+def download_pikachu(data_dir):
+    root_url = ('https://apache-mxnet.s3-accelerate.amazonaws.com/'
+                'gluon/dataset/pikachu/')
+    dataset = {'train.rec': 'e6bcb6ffba1ac04ff8a9b1115e650af56ee969c8',
+               'train.idx': 'dcf7318b2602c06428b9988470c731621716c393',
+               'val.rec': 'd6c33f799b4d058e82f2cb5bd9a976f69d72d520'}
+    for k, v in dataset.items():
+        gluon.utils.download(
+            root_url + k, os.path.join(data_dir, k), sha1_hash=v)
+
+# Defined in file: ./chapter_computer-vision/object-detection-dataset.md
+def load_data_pikachu(batch_size, edge_size=256):
+    """Load the pikachu dataset"""
+    data_dir = '../data/pikachu'
+    download_pikachu(data_dir)
+    train_iter = image.ImageDetIter(
+        path_imgrec=os.path.join(data_dir, 'train.rec'),
+        path_imgidx=os.path.join(data_dir, 'train.idx'),
+        batch_size=batch_size,
+        data_shape=(3, edge_size, edge_size),  # The shape of the output image
+        shuffle=True,  # Read the data set in random order
+        rand_crop=1,  # The probability of random cropping is 1
+        min_object_covered=0.95, max_attempts=200)
+    val_iter = image.ImageDetIter(
+        path_imgrec=os.path.join(data_dir, 'val.rec'), batch_size=batch_size,
+        data_shape=(3, edge_size, edge_size), shuffle=False)
+    return train_iter, val_iter
+
 # Defined in file: ./chapter_generative_adversarial_networks/gan.md
 def update_D(X, Z, net_D, net_G, loss, trainer_D):
     """Update discriminator"""
