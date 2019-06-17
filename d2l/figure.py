e64ae34ac86254310acf280ed693ea0d64a6a40c
muli <muli@cs.cmu.edu>
2019-06-17 05:36:22 +0000

    remove semilogy

--- d2l/figure.py
+++ d2l/figure.py
@@ -5,17 +5,6 @@ from mxnet import nd
 from .d2l import set_figsize, use_svg_display
 import numpy as np
 
-def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
-             legend=None, figsize=(3.5, 2.5)):
-    """Plot x and log(y)."""
-    set_figsize(figsize)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.semilogy(x_vals, y_vals)
-    if x2_vals and y2_vals:
-        plt.semilogy(x2_vals, y2_vals, linestyle=':')
-        plt.legend(legend)
-    plt.show()
 
 def _make_list(obj, default_values=None):
     if obj is None:
