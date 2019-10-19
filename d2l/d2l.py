d07866db84562f9f08ef35f41076b6770f84d0ff
Aston Zhang <asv325@gmail.com>
2019-10-19 01:15:51 +0000

    d2l.py

--- d2l/d2l.py
+++ d2l/d2l.py
@@ -23,6 +23,64 @@ import zipfile
 import pandas as pd
 
 
+# Defined in file: ./chapter_preliminaries/calculus.md
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter."""
+    display.set_matplotlib_formats('svg')
+
+
+# Defined in file: ./chapter_preliminaries/calculus.md
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib."""
+    use_svg_display()
+    d2l.plt.rcParams['figure.figsize'] = figsize
+    
+
+
+# Defined in file: ./chapter_preliminaries/calculus.md
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib."""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+# Defined in file: ./chapter_preliminaries/calculus.md
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=['-', 'm--', 'g-.', 'r:'], figsize=(3.5, 2.5), axes=None):
+    """Plot data points."""
+    d2l.set_figsize(figsize)
+    axes = axes if axes else d2l.plt.gca()
+
+    # Return True if X (ndarray or list) has 1 axis
+    def has_one_axis(X):
+        return (hasattr(X, "ndim") and X.ndim == 1 or isinstance(X, list)
+                and not hasattr(X[0], "__len__"))
+
+    if has_one_axis(X):
+        X = [X]
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+    if len(X) != len(Y):
+        X = X * len(Y)
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        if len(x):
+            axes.plot(x, y, fmt)
+        else:
+            axes.plot(y, fmt)
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+
 # Defined in file: ./chapter_linear-networks/linear-regression.md
 class Timer(object):
     """Record multiple running times."""
