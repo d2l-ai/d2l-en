12942aa653bd33b4b0a864bfc708a47c83bb9e65
Shuai Zhang <cheungdaven@gmail.com>
2019-10-23 23:53:45 +0800

    Update d2l.py

--- d2l/d2l.py
+++ d2l/d2l.py
@@ -1596,7 +1596,7 @@ def evaluate_ranking(net, test_input, seq, candidates, num_users, num_items,
 def train_ranking(net, train_iter, test_iter, loss, trainer, test_seq_iter, 
                   num_users, num_items, num_epochs, ctx_list, evaluator, 
                   negative_sampler, candidates, eval_step=1):
-    num_batches, timer = len(train_iter), d2l.Timer()
+    num_batches, timer, hit_rate, auc  = len(train_iter), d2l.Timer(), 0, 0
     animator = d2l.Animator(xlabel='epoch', xlim=[0, num_epochs], ylim=[0, 1],
                             legend=['test hit rate', 'test AUC'])
     for epoch in range(num_epochs):
