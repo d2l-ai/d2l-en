d8ab5ce5db0b9fe66723ae1ed246948e275e7269
Mu Li <mli@amazon.com>
2019-12-19 12:02:50 -0800

    add logos on the homepage

--- static/build_html.sh
+++ static/build_html.sh
@@ -4,7 +4,7 @@ rm -rf _build/rst _build/html
 d2lbook build rst
 cp static/frontpage/frontpage.html _build/rst/
 d2lbook build html
-cp static/frontpage/_images/* _build/html/_images/
+cp -r static/frontpage/_images/* _build/html/_images/
 
 for fn in `find _build/html/_images/ -iname '*.svg' `; do
     if [[ $fn == *'qr_'* ]] ; then # || [[ $fn == *'output_'* ]]
