d747e13dafc7040f5676298d662e5ffc9c44826a
Anirudh <anirudhdagar6@gmail.com>
2020-05-17 11:38:12 +0530

    Linreg torch (#865)
    * update index imports
    
    * update d2l_pytorch save functions
    
    * pytorch: add linear reg concise

--- d2l_pytorch/d2l_pytorch/__init__.py
+++ d2l_pytorch/d2l_pytorch/__init__.py
@@ -7,8 +7,24 @@ d2l_pytorch = sys.modules[__name__]
 
 # Defined in file: ./chapter_preface/index.md
 import sys
+import collections
+from collections import defaultdict
 from matplotlib import pyplot as plt
 from IPython import display
+import math
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from torch import nn
+import numpy as np
+import os
+import pandas as pd
+import random
+import re
+import shutil
+import sys
+import tarfile
+import time
+import zipfile
 
 d2l = sys.modules[__name__]
 __version__ = '0.5.0'
@@ -73,5 +89,68 @@ def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
         else:
             axes.plot(y, fmt)
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression.md
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        # Start the timer
+        self.tik = time.time()
+
+    def stop(self):
+        # Stop the timer and record the time in a list
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        # Return the average time
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        # Return the sum of time
+        return sum(self.times)
+
+    def cumsum(self):
+        # Return the accumulated times
+        return np.array(self.times).cumsum().tolist()
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-scratch.md
+def synthetic_data(w, b, num_examples):
+    """Generate y = X w + b + noise."""
+    X = torch.zeros(size=(num_examples, len(w))).normal_()
+    y = torch.matmul(X, w) + b
+    y += torch.zeros(size=y.shape).normal_(std=0.01)
+    return X, y
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-scratch.md
+def linreg(X, w, b):
+    return torch.matmul(X, w) + b
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-scratch.md
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-scratch.md
+def sgd(params, lr, batch_size):
+    for param in params:
+        param.data.sub_(lr*param.grad/batch_size)
+        param.grad.data.zero_()
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-gluon.md
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data loader"""
+    dataset = TensorDataset(*data_arrays)
+    dataloader = DataLoader(dataset, batch_size, shuffle=is_train)
+    return dataloader
 
 
