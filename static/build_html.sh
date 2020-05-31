dd2321088957709402734b6b8130d58a01e01d61
muli <muli@cs.cmu.edu>
2020-05-31 03:38:55 +0000

    fix frontpage

--- static/build_html.sh
+++ static/build_html.sh
@@ -2,7 +2,7 @@
 
 rm -rf _build/rst _build/html
 d2lbook build rst --tab all
-cp static/frontpage/frontpage.html _build/rst/
+cp static/frontpage/frontpage.html _build/rst_all/
 d2lbook build html --tab all
 cp -r static/frontpage/_images/* _build/html/_images/
 
