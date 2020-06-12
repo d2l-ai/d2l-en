c64a5ea091fe876cfde8797a5856713508d9fdb7
terrytangyuan <terrytangyuan@gmail.com>
2020-06-12 11:42:59 -0400

    Add section on resnet

--- d2l/tensorflow.py
+++ d2l/tensorflow.py
@@ -496,3 +496,27 @@ def train_ch6(net_fn, train_iter, test_iter, num_epochs, lr,
     return net
 
 
+# Defined in file: ./chapter_convolutional-modern/resnet.md
+class Residual(tf.keras.Model):  #@save
+    def __init__(self, num_channels, use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            num_channels, padding='same', kernel_size=3, strides=strides)
+        self.conv2 = tf.keras.layers.Conv2D(
+            num_channels, kernel_size=3,padding='same')
+        self.conv3 = None
+        if use_1x1conv:
+            self.conv3 = tf.keras.layers.Conv2D(
+                num_channels, kernel_size=1, strides=strides)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn2 = tf.keras.layers.BatchNormalization()
+
+    def call(self, X):
+        Y = tf.keras.activations.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3 is not None:
+            X = self.conv3(X)
+        Y += X
+        return tf.keras.activations.relu(Y + X)
+
+
