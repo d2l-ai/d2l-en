aa014e8a21ac5d81ac5cdd71046e80bc7f5cafdf
Georgios Kaissis <g.kaissis@tum.de>
2020-06-27 12:19:56 +0200

    merge PR1 from Moritz

--- d2l/mxnet.py
+++ d2l/mxnet.py
@@ -99,6 +99,22 @@ class Timer:  #@save
         return np.array(self.times).cumsum().tolist()
 
 
+# Defined in file: ./chapter_linear-networks/linear-regression-scratch.md
+def synthetic_data(w, b, num_examples):  #@save
+    """Generate y = X w + b + noise."""
+    X = np.random.normal(0, 1, (num_examples, len(w)))
+    y = np.dot(X, w) + b
+    y += np.random.normal(0, 0.01, y.shape)
+    return X, y
+
+
+# Defined in file: ./chapter_linear-networks/linear-regression-concise.md
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    """Construct a Gluon data loader."""
+    dataset = gluon.data.ArrayDataset(*data_arrays)
+    return gluon.data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
 # Defined in file: ./chapter_linear-networks/image-classification-dataset.md
 def get_fashion_mnist_labels(labels):  #@save
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
@@ -132,6 +148,38 @@ def get_dataloader_workers(num_workers=4):  #@save
         return num_workers
 
 
+# Defined in file: ./chapter_linear-networks/image-classification-dataset.md
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """Download the Fashion-MNIST dataset and then load into memory."""
+    dataset = gluon.data.vision
+    trans = [dataset.transforms.Resize(resize)] if resize else []
+    trans.append(dataset.transforms.ToTensor())
+    trans = dataset.transforms.Compose(trans)
+    mnist_train = dataset.FashionMNIST(train=True).transform_first(trans)
+    mnist_test = dataset.FashionMNIST(train=False).transform_first(trans)
+    return (gluon.data.DataLoader(mnist_train, batch_size, shuffle=True,
+                                  num_workers=get_dataloader_workers()),
+            gluon.data.DataLoader(mnist_test, batch_size, shuffle=False,
+                                  num_workers=get_dataloader_workers()))
+
+
+# Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
+def accuracy(y_hat, y):  #@save
+    if y_hat.shape[1] > 1:
+        return float((y_hat.argmax(axis=1).astype('float32') == y.astype(
+            'float32')).sum())
+    else:
+        return float((y_hat.astype('int32') == y.astype('int32')).sum())
+
+
+# Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
+def evaluate_accuracy(net, data_iter):  #@save
+    metric = Accumulator(2)  # num_corrected_examples, num_examples
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), y.size)
+    return metric[0] / metric[1]
+
+
 # Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
 class Accumulator:  #@save
     """Sum a list of numbers over time."""
@@ -146,6 +194,23 @@ class Accumulator:  #@save
 
     def __getitem__(self, idx):
         return self.data[idx]
+
+
+# Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
+def train_epoch_ch3(net, train_iter, loss, updater):  #@save
+    metric = Accumulator(3)  # train_loss_sum, train_acc_sum, num_examples
+    if isinstance(updater, gluon.Trainer):
+        updater = updater.step
+    for X, y in train_iter:
+        # Compute gradients and update parameters
+        with autograd.record():
+            y_hat = net(X)
+            l = loss(y_hat, y)
+        l.backward()
+        updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.size)
+    # Return training loss and training accuracy
+    return metric[0]/metric[2], metric[1]/metric[2]
 
 
 # Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
@@ -188,5 +253,103 @@ class Animator:  #@save
         self.config_axes()
         display.display(self.fig)
         display.clear_output(wait=True)
+
+
+# Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs],
+                        ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch+1, train_metrics+(test_acc,))
+
+
+# Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
+def predict_ch3(net, test_iter, n=6): #@save
+    for X, y in test_iter:
+        break
+    trues = d2l.get_fashion_mnist_labels(y)
+    preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true+'\n' + pred for true, pred in zip(trues, preds)]
+    d2l.show_images(X[0:n].reshape(n, 28, 28), 1, n, titles=titles[0:n])
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/underfit-overfit.md
+def evaluate_loss(net, data_iter, loss):  #@save
+    """Evaluate the loss of a model on the given dataset."""
+    metric = d2l.Accumulator(2)  # sum_loss, num_examples
+    for X, y in data_iter:
+        metric.add(loss(net(X), y).sum(), y.size)
+    return metric[0] / metric[1]
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+DATA_HUB = dict()
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+def download(name, cache_dir=os.path.join('..', 'data')):
+    """Download a file inserted into DATA_HUB, return the local filename."""
+    assert name in DATA_HUB, "%s does not exist" % name
+    url, sha1 = DATA_HUB[name]
+    d2l.mkdir_if_not_exist(cache_dir)
+    return gluon.utils.download(url, cache_dir, sha1_hash=sha1)
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+def download_extract(name, folder=None):
+    """Download and extract a zip/tar file."""
+    fname = download(name)
+    base_dir = os.path.dirname(fname) 
+    data_dir, ext = os.path.splitext(fname)
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, 'Only zip/tar files can be extracted'
+    fp.extractall(base_dir)
+    if folder:
+        return os.path.join(base_dir, folder)
+    else:
+        return data_dir
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+def download_all():
+    """Download all files in the DATA_HUB"""
+    for name in DATA_HUB:
+        download(name)
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+DATA_HUB['kaggle_house_train'] = (
+    DATA_URL + 'kaggle_house_pred_train.csv',
+    '585e9cc93e70b39160e7921475f9bcd7d31219ce')
+
+
+# Defined in file: ./chapter_multilayer-perceptrons/kaggle-house-price.md
+DATA_HUB['kaggle_house_test'] = (
+    DATA_URL + 'kaggle_house_pred_test.csv',
+    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+
+
+# Defined in file: ./chapter_deep-learning-computation/use-gpu.md
+def try_gpu(i=0):  #@save
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    return npx.gpu(i) if npx.num_gpus() >= i + 1 else npx.cpu()
+
+
+# Defined in file: ./chapter_deep-learning-computation/use-gpu.md
+def try_all_gpus():  #@save
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    ctxes = [npx.gpu(i) for i in range(npx.num_gpus())]
+    return ctxes if ctxes else [npx.cpu()]
 
 
