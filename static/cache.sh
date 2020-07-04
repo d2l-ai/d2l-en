87d6fd37f82a99ee6449f9ad64ad4fe92c0ce705
stevenjokes <a867907127@gmail.com>
2020-07-04 11:01:23 +0800

    add some spaces to beautify

--- static/cache.sh
+++ static/cache.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-   echo "usage:"
-   echo "  $0 store dir"
-   echo "  $0 restore dir"
-   exit -1
+    echo "usage:"
+    echo "  $0 store dir"
+    echo "  $0 restore dir"
+    exit -1
 fi
 
 cmd=$1
@@ -13,14 +13,14 @@ saved_dir="${dir///data/_data}"
 
 if [ $cmd == "store" ]; then
     if [ -e $dir ]; then
-       rm -rf $saved_dir
-       mv $dir $saved_dir
+        rm -rf $saved_dir
+        mv $dir $saved_dir
     fi
     echo "Saved $dir to $saved_dir"
 elif [ $cmd == "restore" ]; then
     if [ -e $saved_dir ]; then
-       rm -rf $dir
-       mv $saved_dir $dir
+        rm -rf $dir
+        mv $saved_dir $dir
     fi
     echo "Restored $dir from $saved_dir"
 else
