2bd15e9603521535337f4ac5e9ac62413a8c4187
Anirudh Dagar <anirudhdagar6@gmail.com>
2020-09-03 15:32:35 +0530

    [PyTorch] Fix GAN & DCGAN

--- d2l/torch.py
+++ d2l/torch.py
@@ -1229,15 +1229,16 @@ def bbox_to_rect(bbox, color):
 def update_D(X, Z, net_D, net_G, loss, trainer_D):
     """Update discriminator."""
     batch_size = X.shape[0]
-    ones = torch.ones((batch_size, 1), device=X.device)
-    zeros = torch.zeros((batch_size, 1), device=X.device)
+    ones = torch.ones((batch_size,), device=X.device)
+    zeros = torch.zeros((batch_size,), device=X.device)
     trainer_D.zero_grad()
     real_Y = net_D(X)
     fake_X = net_G(Z)
     # Do not need to compute gradient for `net_G`, detach it from
     # computing gradients.
     fake_Y = net_D(fake_X.detach())
-    loss_D = (loss(real_Y, ones) + loss(fake_Y, zeros)) / 2
+    loss_D = (loss(real_Y, ones.reshape(real_Y.shape)) + 
+              loss(fake_Y, zeros.reshape(fake_Y.shape))) / 2
     loss_D.backward()
     trainer_D.step()
     return loss_D
@@ -1247,16 +1248,21 @@ def update_D(X, Z, net_D, net_G, loss, trainer_D):
 def update_G(Z, net_D, net_G, loss, trainer_G):
     """Update generator."""
     batch_size = Z.shape[0]
-    ones = torch.ones((batch_size, 1), device=Z.device)
+    ones = torch.ones((batch_size,), device=Z.device)
     trainer_G.zero_grad()
     # We could reuse `fake_X` from `update_D` to save computation
     fake_X = net_G(Z)
     # Recomputing `fake_Y` is needed since `net_D` is changed
     fake_Y = net_D(fake_X)
-    loss_G = loss(fake_Y,ones)
+    loss_G = loss(fake_Y, ones.reshape(fake_Y.shape))
     loss_G.backward()
     trainer_G.step()
     return loss_G
+
+
+# Defined in file: ./chapter_generative-adversarial-networks/dcgan.md
+d2l.DATA_HUB['pokemon'] = (d2l.DATA_URL + 'pokemon.zip',
+                           'c065c0e2593b8b161a2d7873e42418bf6a21106c')
 
 
 # Alias defined in config.ini
