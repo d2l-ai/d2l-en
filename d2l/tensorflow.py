ba28f20605c6537d447dfd7d65622630b8935642
terrytangyuan <terrytangyuan@gmail.com>
2020-09-25 14:58:33 -0400

    Save get_params
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- d2l/tensorflow.py
+++ d2l/tensorflow.py
@@ -676,6 +676,24 @@ def load_data_time_machine(batch_size, num_steps,  #@save
 
 
 # Defined in file: ./chapter_recurrent-neural-networks/rnn-scratch.md
+def get_params(vocab_size, num_hidden):  #@save
+    num_inputs = num_outputs = vocab_size
+    
+    def normal(shape):
+        return d2l.normal(shape=shape,stddev=0.01,mean=0,dtype=tf.float32)
+
+    # Hidden layer parameters
+    W_xh = tf.Variable(normal((num_inputs, num_hiddens)), dtype=tf.float32)
+    W_hh = tf.Variable(normal((num_hiddens, num_hiddens)), dtype=tf.float32)
+    b_h = tf.Variable(d2l.zeros(num_hiddens), dtype=tf.float32)
+    # Output layer parameters
+    W_hq = tf.Variable(normal((num_hiddens, num_outputs)), dtype=tf.float32)
+    b_q = tf.Variable(d2l.zeros(num_outputs), dtype=tf.float32)
+    params = [W_xh, W_hh, b_h, W_hq, b_q]
+    return params
+
+
+# Defined in file: ./chapter_recurrent-neural-networks/rnn-scratch.md
 class RNNModelScratch: #@save
     """A RNN Model implemented from scratch."""
     def __init__(self, vocab_size, num_hiddens,
