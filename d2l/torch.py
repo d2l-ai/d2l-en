adc5386d0acd181cf2040004f6bd0223ed668dff
Aston Zhang <asv325@gmail.com>
2020-12-09 03:12:07 +0000

    multihead attn to pytorch

--- d2l/torch.py
+++ d2l/torch.py
@@ -1169,54 +1169,50 @@ class MultiHeadAttention(nn.Module):
         self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
         self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
 
-    def forward(self, queries, keys, values, valid_len):
-        # For self-attention, `queries`, `keys`, and `values` shape:
-        # (`batch_size`, `seq_len`, `dim`), where `seq_len` is the length of
-        # input sequence. `valid_len` shape is either (`batch_size`, ) or
-        # (`batch_size`, `seq_len`).
-
-        # Project and transpose `queries`, `keys`, and `values` from
-        # (`batch_size`, `seq_len`, `num_hiddens`) to
-        # (`batch_size` * `num_heads`, `seq_len`, `num_hiddens` / `num_heads`)
+    def forward(self, queries, keys, values, valid_lens):
+        # For self-attention, shape of input `queries`, `keys`, or `values`:
+        # (`batch_size`, `num_steps`, some value). Shape of `valid_len`:
+        # either (`batch_size`,) or (`batch_size`, `num_steps`).
+        # After transposing, shape of output `queries`, `keys`, or `values`: 
+        # (`batch_size` * `num_heads`, `num_steps`,
+        # `num_hiddens` / `num_heads`)
         queries = transpose_qkv(self.W_q(queries), self.num_heads)
         keys = transpose_qkv(self.W_k(keys), self.num_heads)
         values = transpose_qkv(self.W_v(values), self.num_heads)
 
-        if valid_len is not None:
-            # Copy `valid_len` by `num_heads` times
-            if valid_len.ndim == 1:
-                valid_len = valid_len.repeat(self.num_heads)
-            else:
-                valid_len = valid_len.repeat(self.num_heads, 1)
+        if valid_lens is not None:
+            # On axis 0, copy the first item (scalar or vector) for
+            # `num_heads` times, then copy the next item, and so on
+            valid_lens = torch.repeat_interleave(
+                valid_lens, repeats=self.num_heads, dim=0)
 
-        # For self-attention, `output` shape:
-        # (`batch_size` * `num_heads`, `seq_len`, `num_hiddens` / `num_heads`)
-        output = self.attention(queries, keys, values, valid_len)
+        # For self-attention, shape of `output`: (`batch_size` * `num_heads`,
+        # `num_steps`, `num_hiddens` / `num_heads`)
+        output = self.attention(queries, keys, values, valid_lens)
 
-        # `output_concat` shape: (`batch_size`, `seq_len`, `num_hiddens`)
+        # Shape of `output_concat`: (`batch_size`, `num_steps`, `num_hiddens`)
         output_concat = transpose_output(output, self.num_heads)
         return self.W_o(output_concat)
 
 
 # Defined in file: ./chapter_attention-mechanisms/transformer.md
 def transpose_qkv(X, num_heads):
-    # Input `X` shape: (`batch_size`, `seq_len`, `num_hiddens`).
-    # Output `X` shape:
-    # (`batch_size`, `seq_len`, `num_heads`, `num_hiddens` / `num_heads`)
+    # Shape of input `X`: (`batch_size`, `num_steps`, `num_hiddens`).
+    # Shape of output `X`:
+    # (`batch_size`, `num_steps`, `num_heads`, `num_hiddens` / `num_heads`)
     X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
 
-    # `X` shape:
-    # (`batch_size`, `num_heads`, `seq_len`, `num_hiddens` / `num_heads`)
+    # Shape of output `X`:
+    # (`batch_size`, `num_heads`, `num_steps`, `num_hiddens` / `num_heads`)
     X = X.permute(0, 2, 1, 3)
 
-    # `output` shape:
-    # (`batch_size` * `num_heads`, `seq_len`, `num_hiddens` / `num_heads`)
-    output = X.reshape(-1, X.shape[2], X.shape[3])
-    return output
+    # Shape of `output`:
+    # (`batch_size` * `num_heads`, `num_steps`, `num_hiddens` / `num_heads`)
+    return X.reshape(-1, X.shape[2], X.shape[3])
 
 
 def transpose_output(X, num_heads):
-    # A reversed version of `transpose_qkv`
+    """Reverse the operation of `transpose_qkv`"""
     X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
     X = X.permute(0, 2, 1, 3)
     return X.reshape(X.shape[0], X.shape[1], -1)
@@ -1260,8 +1256,8 @@ class EncoderBlock(nn.Module):
             ffn_num_input, ffn_num_hiddens, num_hiddens)
         self.addnorm2 = AddNorm(norm_shape, dropout)
 
-    def forward(self, X, valid_len):
-        Y = self.addnorm1(X, self.attention(X, X, X, valid_len))
+    def forward(self, X, valid_lens):
+        Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
         return self.addnorm2(Y, self.ffn(Y))
 
 
@@ -1281,10 +1277,10 @@ class TransformerEncoder(d2l.Encoder):
                              norm_shape, ffn_num_input, ffn_num_hiddens,
                              num_heads, dropout, use_bias))
 
-    def forward(self, X, valid_len, *args):
+    def forward(self, X, valid_lens, *args):
         X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
         for blk in self.blks:
-            X = blk(X, valid_len)
+            X = blk(X, valid_lens)
         return X
 
 
