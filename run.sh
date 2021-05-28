44247b2ba1088fac6dafe39b9706635a7eff26a9
Mu Li <muli@cs.cmu.edu>
2021-05-28 09:12:09 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-sed -i .bak 's/eval_notebook = True/eval_notebook = False/' config.ini
-for i in {1..100}; do clear; d2lbook build slides --tab pytorch; sleep 10; done
-sed -i .bak 's/eval_notebook = False/eval_notebook = True/' config.ini
