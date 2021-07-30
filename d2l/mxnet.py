7a71aad1621ea10fed639f30fb9c9ad0a873592e
Mu Li <mli@amazon.com>
2021-07-30 11:35:51 -0700

    fix

--- d2l/mxnet.py
+++ d2l/mxnet.py
@@ -24,7 +24,7 @@ from mxnet import autograd, context, gluon, image, init, np, npx
 from mxnet.gluon import nn, rnn
 
 d2l = sys.modules[__name__]
-nn_Module = nn.gluon 
+nn_Module = nn.Block 
 
 #################   WARNING   ################
 # The below part is generated automatically through:
