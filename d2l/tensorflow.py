64d20845a42581901d0951af2c0ea43a5b3ca678
Shuai Zhang <cheungdaven@gmail.com>
2022-02-18 22:02:13 -0800

    fix bug in seq2seq
    revise the call function in the module class.

--- d2l/tensorflow.py
+++ d2l/tensorflow.py
@@ -193,9 +193,9 @@ class Module(d2l.nn_Module, d2l.HyperParameters):
         assert hasattr(self, 'net'), 'Neural network is defined'
         return self.net(X)
 
-    def call(self, X, *args, training=None):
-        if training is not None:
-            self.training = training
+    def call(self,X, *args, **kwargs):
+        if kwargs and "training" in kwargs:
+            self.training = kwargs['training']
         return self.forward(X, *args)
 
     def plot(self, key, value, train):
