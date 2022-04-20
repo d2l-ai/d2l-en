6f6f0fc9d0a8b642eb01f965e46e7498a9970ea7
Aston Zhang <asv325@gmail.com>
2022-04-20 23:17:53 +0000

    fix tf bleu

--- chapter_convolutional-modern/cnn-design.md
+++ chapter_convolutional-modern/cnn-design.md
@@ -440,7 +440,7 @@ characterized by
 class RegNet32(AnyNet):
     def __init__(self, lr=0.1, num_classes=10):
         stem_channels, groups, bot_mul = 32, 16, 1
-        depths, channels = [4, 6], [32, 80]
+        depths, channels = (4, 6), (32, 80)
         if tab.selected(['mxnet']):
             super().__init__(
                 ((depths[0], channels[0], groups, bot_mul),
--- d2l/mxnet.py
+++ d2l/mxnet.py
@@ -3155,24 +3155,6 @@ def load_data_nmt(batch_size, num_steps, num_examples=600):
     data_iter = d2l.load_array(data_arrays, batch_size)
     return data_iter, src_vocab, tgt_vocab
 
-def bleu(pred_seq, label_seq, k):
-    """Compute the BLEU.
-
-    Defined in :numref:`sec_utils`"""
-    pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
-    len_pred, len_label = len(pred_tokens), len(label_tokens)
-    score = math.exp(min(0, 1 - len_label / len_pred))
-    for n in range(1, k + 1):
-        num_matches, label_subs = 0, collections.defaultdict(int)
-        for i in range(len_label - n + 1):
-            label_subs[''.join(label_tokens[i: i + n])] += 1
-        for i in range(len_pred - n + 1):
-            if label_subs[''.join(pred_tokens[i: i + n])] > 0:
-                num_matches += 1
-                label_subs[''.join(pred_tokens[i: i + n])] -= 1
-        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
-    return score
-
 class MaskedSoftmaxCELoss(gluon.loss.SoftmaxCELoss):
     """The softmax cross-entropy loss with masks.
 
