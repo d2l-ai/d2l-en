a982f9e79b03a74d0eccd8a70f3e068e7b47df93
Aston Zhang <22279212+astonzhang@users.noreply.github.com>
2022-04-25 12:39:00 -0700

    Update torch.py

--- d2l/torch.py
+++ d2l/torch.py
@@ -35,7 +35,6 @@ import requests
 from IPython import display
 from matplotlib import pyplot as plt
 from matplotlib_inline import backend_inline
-from sklearn import model_selection
 
 d2l = sys.modules[__name__]
 
