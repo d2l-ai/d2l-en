29099280b71a6e68b1979afe381fc8f3d64162c2
Aston Zhang <asv325@gmail.com>
2022-07-29 17:32:56 +0000

    sync lib

--- d2l/torch.py
+++ d2l/torch.py
@@ -288,7 +288,7 @@ class Trainer(d2l.HyperParameters):
             self.optim.zero_grad()
             with torch.no_grad():
                 loss.backward()
-                if self.gradient_clip_val > 0:
+                if self.gradient_clip_val > 0:  # To be discussed later
                     self.clip_gradients(self.gradient_clip_val, self.model)
                 self.optim.step()
             self.train_batch_idx += 1
